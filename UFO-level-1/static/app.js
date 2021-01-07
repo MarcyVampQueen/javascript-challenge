ccfb208610b27d1edd360d6124e9c3e387ade194
MarcyVampQueen <savannahcordry@gmail.com>
2021-01-06 22:04:14 -0800

    basic table structure created

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,4 +1,24 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// // Append one table row `tr` to the table body
+// var row = tbody.append("tr");
+
+// // Append one cell for the student name
+// row.append("td").text(newGrade[0]);
+
+// // Append one cell for the student grade
+// row.append("td").text(newGrade[1]);
+
+// Loop through the data to build the table
+for (let sighting of tableData) {
+    var row = tbody.append("tr");
+    row.append("td").text(sighting.datetime);
+    console.log(sighting.datetime);
+    row.append("td").text(sighting.city);
+    row.append("td").text(sighting.state);
+    row.append("td").text(sighting.country);
+    row.append("td").text(sighting.shape);
+    row.append("td").text(sighting.comments);
+}
